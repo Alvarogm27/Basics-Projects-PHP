c730e9f4e58979c7225d36c92ac7e9eee62dfc10
Alvarogm27 <alvarogarciamarquez.ccg@gmail.com>
2024-11-18 22:52:14 +0100

    calculation.php Declaracion de las variables del formulario

---
+++ Proyecto_calculadora_mensual/calculation.php
@@ -0,0 +1,9 @@
+<?php
+//Declaramos los valores que vamos a obtener del formulario en  forma de variables para que sea mas sencillo de operar
+
+$ingresos = $_POST['ingresos'];
+$alquiler = $_POST['alquiler'];
+$comida = $_POST['comida'];
+$transporte = $_POST['transporte'];
+$entretenimiento = $_POST['entretenimiento'];
+
